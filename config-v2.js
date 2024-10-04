a361ac1d27874082dd7d3f5853001b562dbcc6fc
panda-sl <binulyas@gmail.com>
2024-10-04 20:14:45 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
-const GITHUB_AUTH_TOKEN = "null"
-const BOT_NUMBER = "null"
-const SESSION_ID = ""
+const GITHUB_AUTH_TOKEN = "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"
+const BOT_NUMBER = "94759404990"
+const SESSION_ID = "PRABATH-MD~nfhgSSKZ#4NZJW1cYGE_iYuLUmRi4gWF8YWE3FOKeopbwbYZ8HlU"
 
 
 module.exports = {
